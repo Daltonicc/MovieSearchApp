c69e46e20dffca2626b464bef1cbaa3220cd66bb
EthanPark <ethanpark604@gmail.com>
2022-04-15 22:41:58 +0900

    Add MovieData Model

---
+++ MovieSearchApp/Model/MovieData.swift
@@ -0,0 +1,22 @@
+//
+//  MovieData.swift
+//  MovieSearchApp
+//
+//  Created by 박근보 on 2022/04/15.
+//
+
+import Foundation
+
+struct MovieData: Codable {
+    let lastBuildDate: String
+    let total, start, display: Int
+    let items: [Item]
+}
+
+struct Item: Codable {
+    let title: String
+    let link: String
+    let image: String
+    let subtitle, pubDate, director, actor: String
+    let userRating: String
+}
---
+++ MovieSearchApp/Network/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  MovieSearchApp
+//
+//  Created by 박근보 on 2022/04/15.
+//
+
+import Foundation
