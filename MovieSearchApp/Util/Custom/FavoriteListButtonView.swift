adc1083263f043443e8b401aa593733599ffed75
EthanPark <ethanpark604@gmail.com>
2022-04-15 12:36:04 +0900

    Add Custom FavoriteButtonListView

---
+++ MovieSearchApp/Util/Custom/FavoriteListButtonView.swift
@@ -0,0 +1,51 @@
+//
+//  FavoriteListButtonView.swift
+//  MovieSearchApp
+//
+//  Created by 박근보 on 2022/04/15.
+//
+
+import UIKit
+import SnapKit
+
+final class FavoriteListButtonView: BaseView {
+
+    let starImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleToFill
+        imageView.image = UIImage(systemName: "star.fill")
+        imageView.tintColor = .yellow
+        return imageView
+    }()
+    let favoriteLabel: UILabel = {
+        let label = UILabel()
+        label.text = "즐겨찾기"
+        label.font = .systemFont(ofSize: 16)
+        return label
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func setUpView() {
+
+        addSubview(starImageView)
+        addSubview(favoriteLabel)
+    }
+
+    override func setUpConstraint() {
+
+        starImageView.snp.makeConstraints { make in
+            make.top.leading.bottom.equalToSuperview().inset(5)
+        }
+        favoriteLabel.snp.makeConstraints { make in
+            make.top.trailing.bottom.equalToSuperview().inset(5)
+            make.leading.equalTo(starImageView.snp.trailing).offset(5)
+        }
+    }
+}
