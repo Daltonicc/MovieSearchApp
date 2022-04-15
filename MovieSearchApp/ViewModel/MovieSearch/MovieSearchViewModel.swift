d6b41cb9e7ce7fd2e7d54a28456139cfabe00e33
EthanPark <ethanpark604@gmail.com>
2022-04-15 17:40:26 +0900

    Add MovieSearchViewModel

--- MovieSearchApp/ViewModel/MovieSearch/MovieSearchViewModel.swift
+++ MovieSearchApp/ViewModel/MovieSearch/MovieSearchViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class MovieSearchViewModel: ViewModelType {
+
+    struct Input {
+        let searchTextFieldBegin: Signal<Void>
+        let searchTextFieldEnd: Signal<Void>
+        let requestMovieListEvent: Signal<Void>
+        let pressFavoriteButtonList: Signal<Void>
+    }
+
+    struct Output {
+        let searchQuery: Signal<String>
+        let showToastAction: Signal<String>
+        let indicatorAction: Driver<Bool>
+    }
+
+    private let searchQuery = PublishRelay<String>()
+    private let showToastAction = PublishRelay<String>()
+    private let indicatorAction = BehaviorRelay<Bool>(value: false)
+
+    var disposeBag = DisposeBag()
+
+    func transform(input: Input) -> Output {
+        
+    }
+}
+
+extension MovieSearchViewModel {
+
+}
