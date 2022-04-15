074144aa903291830af5e7dfe0508befa2b90d62
EthanPark <ethanpark604@gmail.com>
2022-04-15 17:39:37 +0900

    MovieTableViewCell UI

--- MovieSearchApp/View/MovieSearch/MovieTableViewCell.swift
+++ MovieSearchApp/View/MovieSearch/MovieTableViewCell.swift
@@ -5,4 +5,36 @@
 //  Created by 박근보 on 2022/04/15.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+final class MovieTableViewCell: UITableViewCell {
+
+    let cellView: MovieInfoView = {
+        let view = MovieInfoView()
+        return view
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setUpView()
+        setUpConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    private func setUpView() {
+        
+        contentView.addSubview(cellView)
+    }
+
+    private func setUpConstraints() {
+
+        cellView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+}
