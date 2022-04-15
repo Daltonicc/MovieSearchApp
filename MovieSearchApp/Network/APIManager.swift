3eed49c563c69bf2befa24ff4a4b15106c427678
EthanPark <ethanpark604@gmail.com>
2022-04-15 22:42:30 +0900

    Add APIManager, NetworkError

--- MovieSearchApp/Network/APIManager.swift
+++ MovieSearchApp/Network/APIManager.swift
@@ -6,3 +6,71 @@
 //
 
 import Foundation
+import Alamofire
+
+enum MovieAPI {
+    case getMovieData(query: String, start: String, display: String)
+}
+
+extension MovieAPI {
+
+    var url: URL {
+        return URL(string: "https://openapi.naver.com/v1/search/movie.json")!
+    }
+
+    var parameters: [String: String] {
+        switch self {
+        case .getMovieData(let query, let start, let display):
+            return [
+                "query": query,
+                "display": display,
+                "start": start
+            ]
+        }
+    }
+
+    var headers: HTTPHeaders {
+        switch self {
+        default:
+            return [
+                "X-Naver-Client-Id": APIKey.clientID,
+                "X-Naver-Client-Secret": APIKey.clientSecret
+            ]
+        }
+    }
+}
+
+final class APIManager {
+
+    static let shared = APIManager()
+
+    func getMovieData(query: String, start: String, display: String, completion: @escaping (Result<(MovieData), MovieError>) -> Void) {
+
+        let movieAPI: MovieAPI = .getMovieData(query: query, start: start, display: display)
+
+        AF.request(movieAPI.url, method: .get, parameters: movieAPI.parameters, headers: movieAPI.headers)
+            .validate()
+            .responseDecodable(of: MovieData.self) { [weak self] response in
+                switch response.result {
+                case .success(let data):
+                    completion(.success(data))
+                case .failure(_):
+                    guard let statusCode = response.response?.statusCode else { return }
+                    guard let error = self?.statusCodeCheck(statusCode: statusCode) else { return }
+                    completion(.failure(error))
+                }
+            }
+    }
+
+    private func statusCodeCheck(statusCode: Int) -> MovieError {
+        switch statusCode {
+        case 400: return .incorrectQuery
+        case 401: return .failAuthorization
+        case 403: return .serverError
+        case 404: return .invalidAPI
+        case 429: return .tooMuchRequest
+        case 500: return .systemError
+        default: return .systemError
+        }
+    }
+}
--- MovieSearchApp/Network/NetworkError.swift
+++ MovieSearchApp/Network/NetworkError.swift
@@ -1,8 +1,30 @@
 //
-//  MovieError.swift
+//  NetworkError.swift
 //  MovieSearchApp
 //
 //  Created by 박근보 on 2022/04/15.
 //
 
 import Foundation
+
+enum MovieError: Int, Error {
+    case incorrectQuery = 400
+    case failAuthorization = 401
+    case serverError = 403
+    case invalidAPI = 404
+    case tooMuchRequest = 429
+    case systemError = 500
+}
+
+extension MovieError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .incorrectQuery: return "검색어를 다시 확인해주세요"
+        case .failAuthorization: return "사용자 인증에 실패했습니다"
+        case .serverError: return "서버 에러입니다"
+        case .invalidAPI: return "존재하지 않는 검색 API입니다"
+        case .tooMuchRequest: return "허용량을 초과했습니다"
+        case .systemError: return "시스템 에러입니다"
+        }
+    }
+}
