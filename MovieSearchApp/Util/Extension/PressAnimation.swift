aec1ed2676735e76892adb225efc0d97f0a0cb7f
EthanPark <ethanpark604@gmail.com>
2022-04-16 20:10:14 +0900

    Edit PressAnimation

--- MovieSearchApp/Util/Extension/PressAnimation.swift
+++ MovieSearchApp/Util/Extension/PressAnimation.swift
@@ -1,5 +1,5 @@
 //
-//  UIViewController+Extension.swift
+//  PressAnimation.swift
 //  MovieSearchApp
 //
 //  Created by 박근보 on 2022/04/16.
@@ -7,15 +7,15 @@
 
 import UIKit
 
-func addPressAnimationToButton(_ viewToAnimate: UIView, completion: ((Bool) -> Void)?) {
+func addPressAnimationToButton(scale: Double, _ viewToAnimate: UIView, completion: ((Bool) -> Void)?) {
 
-    UIView.animate(withDuration: 0.05, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+    UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
 
-        viewToAnimate.transform = CGAffineTransform(scaleX: 0.95, y: 0.95)
+        viewToAnimate.transform = CGAffineTransform(scaleX: scale, y: scale)
 
     }) { _ in
 
-        UIView.animate(withDuration: 0.05, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
 
             viewToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
 
