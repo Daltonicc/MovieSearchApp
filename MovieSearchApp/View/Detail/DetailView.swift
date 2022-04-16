3644ddd1ccc87702556d6c0e1ded570682a8551e
EthanPark <ethanpark604@gmail.com>
2022-04-16 21:19:52 +0900

    Add DetailView UI and Logic

--- MovieSearchApp/View/Detail/DetailView.swift
+++ MovieSearchApp/View/Detail/DetailView.swift
@@ -5,4 +5,57 @@
 //  Created by 박근보 on 2022/04/16.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+import WebKit
+
+final class DetailView: BaseView {
+
+    let movieView: MovieInfoView = {
+        let view = MovieInfoView()
+        return view
+    }()
+    let separateView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemGray5
+        return view
+    }()
+    let webView: WKWebView = {
+        let webView = WKWebView()
+        return webView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    override func setUpView() {
+
+        addSubview(separateView)
+        addSubview(movieView)
+        addSubview(webView)
+    }
+
+    override func setUpConstraint() {
+
+        separateView.snp.makeConstraints { make in
+            make.top.equalTo(self.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(5)
+        }
+        movieView.snp.makeConstraints { make in
+            make.top.equalTo(separateView.snp.bottom)
+            make.leading.trailing.equalToSuperview().inset(10)
+            make.height.equalTo(110)
+        }
+        webView.snp.makeConstraints { make in
+            make.top.equalTo(movieView.snp.bottom)
+            make.leading.trailing.bottom.equalToSuperview()
+        }
+    }
+}
--- MovieSearchApp/View/Detail/DetailViewController.swift
+++ MovieSearchApp/View/Detail/DetailViewController.swift
@@ -5,4 +5,78 @@
 //  Created by 박근보 on 2022/04/16.
 //
 
-import Foundation
+import UIKit
+import WebKit
+import RxSwift
+import RxCocoa
+
+final class DetailViewController: BaseViewController {
+
+    private let mainView = DetailView()
+
+    var movieItem: MovieItem?
+
+    override func loadView() {
+        self.view = mainView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        topViewConfig()
+        webViewConfig()
+    }
+
+    override func setViewConfig() {
+        super.setViewConfig()
+    }
+
+    override func navigationItemConfig() {
+
+        mainView.backBarButton.target = self
+        mainView.backBarButton.action = #selector(backButtonClicked(sender:))
+        navigationItem.leftBarButtonItem = mainView.backBarButton
+    }
+
+    private func topViewConfig() {
+
+        guard let movieItem = movieItem else { return }
+
+        if let imageurl = URL(string: movieItem.image) {
+            mainView.movieView.posterImageView.kf.setImage(with: imageurl)
+        } else {
+            mainView.movieView.posterImageView.image = UIImage(systemName: "star")
+        }
+
+        let title = movieItem.title
+            .replacingOccurrences(of: "<b>", with: "")
+            .replacingOccurrences(of: "</b>", with: "")
+        var director = movieItem.director
+            .replacingOccurrences(of: "|", with: ", ").dropLast()
+        var actor = movieItem.actor
+            .replacingOccurrences(of: "|", with: ", ").dropLast()
+        let rate = movieItem.userRating
+
+        if director.count >= 1 {
+            director.removeLast()
+        }
+        if actor.count >= 1 {
+            actor.removeLast()
+        }
+        navigationItem.title = title
+        mainView.movieView.titleLabel.text = title
+        mainView.movieView.directorLabel.text = "감독: \(director)"
+        mainView.movieView.actorLabel.text = "출연: \(actor)"
+        mainView.movieView.rateLabel.text = "평점: \(rate)"
+    }
+
+    private func webViewConfig() {
+        guard let movieItem = movieItem else { return }
+        guard let url = URL(string: movieItem.link) else { return }
+        let request = URLRequest(url: url)
+        mainView.webView.load(request)
+    }
+
+    @objc func backButtonClicked(sender: UIBarButtonItem) {
+        navigationController?.popViewController(animated: true)
+    }
+}
