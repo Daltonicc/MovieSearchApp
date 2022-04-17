0d6b0a93c1387fb2e2bef3b5405c88ebdd0cbf16
EthanPark <ethanpark604@gmail.com>
2022-04-17 18:27:55 +0900

    Add MovieTableViewCellDelegate

--- MovieSearchApp/View/Cell/MovieTableViewCell.swift
+++ MovieSearchApp/View/Cell/MovieTableViewCell.swift
@@ -9,6 +9,10 @@ import UIKit
 import SnapKit
 import Kingfisher
 
+protocol MovieTableViewCellDelegate: AnyObject {
+    func didTapFavoriteButton(tag: Int, status: Bool)
+}
+
 final class MovieTableViewCell: UITableViewCell {
 
     let cellView: MovieInfoView = {
@@ -16,11 +20,17 @@ final class MovieTableViewCell: UITableViewCell {
         return view
     }()
 
+    var isFavorite = false
+
+    weak var delegate: MovieTableViewCellDelegate?
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
         setUpView()
         setUpConstraints()
+
+        cellView.favoriteButton.addTarget(self, action: #selector(favoriteButtonTap(sender:)), for: .touchUpInside)
     }
 
     required init?(coder: NSCoder) {
@@ -39,7 +49,7 @@ final class MovieTableViewCell: UITableViewCell {
         }
     }
 
-    func cellConfig(movieItem: MovieItem) {
+    func cellConfig(movieItem: MovieItem, tag: Int) {
 
         if let imageurl = URL(string: movieItem.image) {
             cellView.posterImageView.kf.setImage(with: imageurl)
@@ -51,22 +61,33 @@ final class MovieTableViewCell: UITableViewCell {
             .replacingOccurrences(of: "<b>", with: "")
             .replacingOccurrences(of: "</b>", with: "")
         var director = movieItem.director
-            .replacingOccurrences(of: "|", with: ", ")
+            .replacingOccurrences(of: "|", with: ", ").dropLast()
         var actor = movieItem.actor
-            .replacingOccurrences(of: "|", with: ", ")
+            .replacingOccurrences(of: "|", with: ", ").dropLast()
         let rate = movieItem.userRating
 
         if director.count >= 1 {
             director.removeLast()
-            director.removeLast()
         }
         if actor.count >= 1 {
             actor.removeLast()
-            actor.removeLast()
         }
+        isFavorite = movieItem.isFavorite
+        
+        cellView.favoriteButton.tintColor = movieItem.isFavorite ? .systemYellow : .systemGray3
         cellView.titleLabel.text = title
         cellView.directorLabel.text = "감독: \(director)"
         cellView.actorLabel.text = "출연: \(actor)"
         cellView.rateLabel.text = "평점: \(rate)"
+        cellView.favoriteButton.tag = tag
+    }
+
+    @objc private func favoriteButtonTap(sender: UIButton) {
+        addPressAnimationToButton(scale: 0.85, cellView.favoriteButton) { [weak self] _ in
+            guard let self = self else { return }
+            self.isFavorite.toggle()
+            self.cellView.favoriteButton.tintColor = self.isFavorite ? .systemYellow : .systemGray3
+            self.delegate?.didTapFavoriteButton(tag: sender.tag, status: self.isFavorite)
+        }
     }
 }
