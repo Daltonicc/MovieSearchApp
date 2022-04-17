bcf0a0b0f9f7f9af74b315563feac3b24aa191ce
EthanPark <ethanpark604@gmail.com>
2022-04-18 02:32:10 +0900

    Edit .gitignore

---
+++ MovieSearchApp/Network/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  MovieSearchApp
+//
+//  Created by 박근보 on 2022/04/15.
+//
+
+import Foundation
+
+class APIKey {
+    static let clientID = "FBAeES11vzmYievJ_ocV"
+    static let clientSecret = "DLjCcp4V3M"
+}
