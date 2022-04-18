9a4f4b56df869e8666d712e24253ee8f112fe6ed
EthanPark <ethanpark604@gmail.com>
2022-04-18 21:15:03 +0900

    Add APIKey

---
+++ MovieSearchApp/Network/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  MovieSearchApp
+//
+//  Created by 박근보 on 2022/04/15.
+//
+
+import Foundation
+
+class APIKey {
+    static let clientID = "클라이언트 ID를 입력해주세요"
+    static let clientSecret = "클라이언트 패스워드를 입력해주세요"
+}
